8a68abf19096301dc981f3828f7fd5a7871699e9
Tien Minh <binpro113wer@gmail.com>
2022-01-12 19:29:25 +0700

    nothing

--- src/Character/Item.js
+++ src/Character/Item.js
@@ -2,11 +2,10 @@ import { NavLink } from "react-router-dom";
 import { GIT_PROJECT } from "../Constant/Constant";
 
 function Item({ name, children }) {
-  console.log(`/${GIT_PROJECT}/characters/${name}`)
   return (
     <>
       <div className="col item">
-        <NavLink /*to={`/${GIT_PROJECT}/characters/${name}`}*/ to={`${GIT_PROJECT}/map`}>
+        <NavLink to={`/${GIT_PROJECT}/characters/${name}`} /*to={`${GIT_PROJECT}/map`}*/ >
           <div className="item__container">
             {children}
             <div className="item__text">
